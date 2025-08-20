887d8bd05a6639bc0aec2110a4565585f5bb81fe
DrRedDustStuff <mrbuildgd@gmail.com>
2025-08-20 22:19:59 +0200

    Changed the __init__.py file to include correct importing to make the usage better and make stuff more findable.

---
+++ src/ue4ss_installer_gui/__init__.py
@@ -0,0 +1,19 @@
+# import all the libraries so you can see them correctly
+# when trying to find something
+
+import auto_align
+import constants
+import data_structures
+import epic
+import file_io
+import font
+import grid
+import initialization
+import logger
+import main
+import settings
+import steam
+import theme_management
+import translator
+import ue4ss
+import unreal_engine
